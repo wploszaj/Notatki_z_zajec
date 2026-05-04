ad6198bf9037aecf5270c341e9f6c65bd17a3d7e
wploszaj <w.ploszaj@student.uw.edu.pl>
2026-05-04 17:29:45 +0200

    Add files via upload

---
+++ Zajecia_10/Zaj_10.BoW_Przestrzen_wektorowa_LDA_rozne_k_Plik_CSV.R
@@ -0,0 +1,299 @@
+
+
+
+#' ---
+#' title: "Modelowanie tematów LDA"
+#' author: "Weronika Płoszaj"
+#' date:   "04.05.2026"
+#' output:
+#'   html_document:
+#'     df_print: paged
+#'     theme: readable      # Wygląd (bootstrap, cerulean, darkly, journal, lumen, paper, readable, sandstone, simplex, spacelab, united, yeti)
+#'     highlight: kate      # Kolorowanie składni (haddock, kate, espresso, breezedark)
+#'     toc: true            # Spis treści
+#'     toc_depth: 3
+#'     toc_float:
+#'       collapsed: false
+#'       smooth_scroll: true
+#'     code_folding: show    
+#'     number_sections: false # Numeruje nagłówki (lepsza nawigacja)
+#' ---
+
+
+knitr::opts_chunk$set(
+  message = FALSE,
+  warning = FALSE
+)
+
+
+
+
+
+#' # Wymagane pakiety
+# Wymagane pakiety ----
+# install.packages(c("tm","tidyverse","tidytext","topicmodels","wordcloud"))
+# install.packages("reshape2")
+library(reshape2)
+library(tm)
+library(tidyverse)
+library(tidytext)
+library(topicmodels)
+library(wordcloud)
+
+
+
+#' # 0. Funkcja top_terms_by_topic_LDA
+# 0. Funkcja top_terms_by_topic_LDA ----
+# która wczytuje tekst 
+# (wektor lub kolumna tekstowa z ramki danych)
+# i wizualizuje słowa o największej informatywności
+# przy metody użyciu LDA
+# dla wyznaczonej liczby tematów
+
+
+
+top_terms_by_topic_LDA <- function(input_text, # wektor lub kolumna tekstowa z ramki danych
+                                   plot = TRUE, # domyślnie rysuje wykres
+                                   k = number_of_topics) # wyznaczona liczba k tematów
+{    
+  corpus <- VCorpus(VectorSource(input_text))
+  DTM <- DocumentTermMatrix(corpus)
+  
+  # usuń wszystkie puste wiersze w macierzy częstości
+  # ponieważ spowodują błąd dla LDA
+  unique_indexes <- unique(DTM$i) # pobierz indeks każdej unikalnej wartości
+  DTM <- DTM[unique_indexes,]    # pobierz z DTM podzbiór tylko tych unikalnych indeksów
+  
+  # wykonaj LDA
+  lda <- LDA(DTM, k = number_of_topics, control = list(seed = 1234))
+  topics <- tidy(lda, matrix = "beta") # pobierz słowa/tematy w uporządkowanym formacie tidy
+  
+  # pobierz dziesięć najczęstszych słów dla każdego tematu
+  top_terms <- topics  %>%
+    group_by(topic) %>%
+    top_n(10, beta) %>%
+    ungroup() %>%
+    arrange(topic, -beta) # uporządkuj słowa w malejącej kolejności informatywności
+
+    
+  
+  # rysuj wykres (domyślnie plot = TRUE)
+  if(plot == T){
+    # dziesięć najczęstszych słów dla każdego tematu
+    top_terms %>%
+      mutate(term = reorder(term, beta)) %>% # posortuj słowa według wartości beta 
+      ggplot(aes(term, beta, fill = factor(topic))) + # rysuj beta według tematu
+      geom_col(show.legend = FALSE) + # wykres kolumnowy
+      facet_wrap(~ topic, scales = "free") + # każdy temat na osobnym wykresie
+      labs(x = "Terminy", y = "β (ważność słowa w temacie)") +
+      coord_flip() +
+      theme_minimal() +
+      scale_fill_brewer(palette = "Set1")
+  }else{ 
+    # jeśli użytkownik nie chce wykresu
+    # wtedy zwróć listę posortowanych słów
+    return(top_terms)
+  }
+  
+  
+}
+
+
+
+
+#' # Dane tekstowe
+# Dane tekstowe ----
+# Ustaw Working Directory!
+# Załaduj dokumenty z folderu
+# docs <- DirSource("textfolder2")
+# W razie potrzeby dostosuj ścieżkę
+# np.: docs <- DirSource("C:/User/Documents/textfolder2")
+
+
+# Utwórz korpus dokumentów tekstowych
+
+# Gdy tekst znajduje się w jednym pliku csv:
+data <- read.csv("LOT_reviews.csv", stringsAsFactors = FALSE, encoding = "UTF-8")
+corpus <- VCorpus(VectorSource(data$Review_Text))
+
+# Korpus
+# inspect(corpus)
+
+
+# Korpus - zawartość przykładowego elementu
+corpus[[1]]
+corpus[[1]][[1]]
+corpus[[1]][2]
+
+
+
+#' # 1. Przetwarzanie i oczyszczanie tekstu
+# 1. Przetwarzanie i oczyszczanie tekstu ----
+# (Text Preprocessing and Text Cleaning)
+
+
+# Normalizacja i usunięcie zbędnych znaków ----
+
+# Zapewnienie kodowania w całym korpusie
+corpus <- tm_map(corpus, content_transformer(function(x) iconv(x, to = "UTF-8", sub = "byte")))
+
+
+# Funkcja do zamiany znaków na spację
+toSpace <- content_transformer(function (x, pattern) gsub(pattern, " ", x))
+
+
+# Usuń zbędne znaki lub pozostałości url, html itp.
+
+# symbol @
+corpus <- tm_map(corpus, toSpace, "@")
+
+# symbol @ ze słowem (zazw. nazwa użytkownika)
+corpus <- tm_map(corpus, toSpace, "@\\w+")
+
+# linia pionowa
+corpus <- tm_map(corpus, toSpace, "\\|")
+
+# tabulatory
+corpus <- tm_map(corpus, toSpace, "[ \t]{2,}")
+
+# CAŁY adres URL:
+corpus <- tm_map(corpus, toSpace, "(s?)(f|ht)tp(s?)://\\S+\\b")
+
+# http i https
+corpus <- tm_map(corpus, toSpace, "http\\w*")
+
+# tylko ukośnik odwrotny (np. po http)
+corpus <- tm_map(corpus, toSpace, "/")
+
+# pozostałość po re-tweecie
+corpus <- tm_map(corpus, toSpace, "(RT|via)((?:\\b\\W*@\\w+)+)")
+
+# inne pozostałości
+corpus <- tm_map(corpus, toSpace, "www")
+corpus <- tm_map(corpus, toSpace, "~")
+corpus <- tm_map(corpus, toSpace, "â€“")
+
+
+# Sprawdzenie
+corpus[[1]][[1]]
+
+corpus <- tm_map(corpus, content_transformer(tolower))
+corpus <- tm_map(corpus, removeNumbers)
+corpus <- tm_map(corpus, removeWords, stopwords("english"))
+corpus <- tm_map(corpus, removePunctuation)
+corpus <- tm_map(corpus, stripWhitespace)
+
+
+# Sprawdzenie
+corpus[[1]][[1]]
+
+corpus <- tm_map(corpus, removeWords, c("flight","flights","lot"))
+corpus <- tm_map(corpus, stripWhitespace)
+
+# Sprawdzenie
+corpus[[1]][[1]]
+
+
+
+# Decyzja dotycząca korpusu ----
+# do dalszej analizy użyj:
+#
+# - corpus (oryginalny, bez stemmingu)
+#
+
+
+
+
+#' # Tokenizacja
+# Tokenizacja ----
+
+
+
+# Macierz częstości TDM ----
+
+tdm <- TermDocumentMatrix(corpus)
+tdm_m <- as.matrix(tdm)
+
+
+
+#' # 2. Zliczanie częstości słów
+# 2. Zliczanie częstości słów ----
+# (Word Frequency Count)
+
+
+# Zlicz same częstości słów w macierzach
+v <- sort(rowSums(tdm_m), decreasing = TRUE)
+tdm_df <- data.frame(word = names(v), freq = v)
+head(tdm_df, 10)
+
+
+
+#' # 3. Eksploracyjna analiza danych
+# 3. Eksploracyjna analiza danych ----
+# (Exploratory Data Analysis, EDA)
+
+
+# Chmura słów (globalna)
+wordcloud(words = tdm_df$word, freq = tdm_df$freq, min.freq = 7, 
+          colors = brewer.pal(8, "Dark2"))
+
+
+# Wyświetl top 10
+print(head(tdm_df, 10))
+
+
+
+#' # 4. Inżynieria cech w modelu Bag of Words:
+#' # Reprezentacja słów i dokumentów w przestrzeni wektorowej
+# 4. Inżynieria cech w modelu Bag of Words: ----
+# Reprezentacja słów i dokumentów w przestrzeni wektorowej ----
+# (Feature Engineering in vector-space BoW model)
+
+# - podejście surowych częstości słów
+# (częstość słowa = liczba wystąpień w dokumencie)
+# (Raw Word Counts)
+
+
+
+#' # UCZENIE MASZYNOWE NIENADZOROWANE
+# UCZENIE MASZYNOWE NIENADZOROWANE ----
+# (Unsupervised Machine Learning)
+
+
+
+
+#' # Modelowanie tematów: ukryta alokacja Dirichleta
+# Modelowanie tematów: ukryta alokacja Dirichleta (LDA) ----
+
+
+
+
+# Rysuj dziesięć słów 
+# o największej informatywności według tematu
+# dla wyznaczonej liczby tematów 
+
+
+# Dobór liczby tematów
+number_of_topics = 2
+top_terms_by_topic_LDA(tdm_df$word)
+
+
+# Zmień wyznaczoną liczbę tematów
+number_of_topics = 3
+top_terms_by_topic_LDA(tdm_df$word)
+
+
+# Zmień wyznaczoną liczbę tematów
+number_of_topics = 4
+top_terms_by_topic_LDA(tdm_df$word)
+
+
+# Zmień wyznaczoną liczbę tematów
+number_of_topics = 6
+top_terms_by_topic_LDA(tdm_df$word)
+
+
+
+
+
+
